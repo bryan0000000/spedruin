d280ab18f69c68e3862d74caaf1cda2059494cdc
spedruin <borelabryan947@gmail.com>
2024-10-31 09:07:10 -0300

    borabryan

---
+++ spedruin/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace spedruin;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ spedruin/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace spedruin;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+}
+
+
